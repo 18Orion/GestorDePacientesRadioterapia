1edbf1a15a1b73659cb7f54538edddc241fe0f43
Sergio <sergiominanor@gmail.com>
2024-08-28 10:35:45 +0200

    Modularized update script

--- libs/funcs.py
+++ libs/funcs.py
@@ -51,3 +51,19 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     # Print New Line on Complete
     if iteration == total: 
         print()
+
+def update(packageType, version="latest"):
+    if version=="latest":
+        version = (requests.get("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest").url.split("/").pop(), system())
+    else:
+        version = (version, system())
+    print("Descargando version "+version[0]+" para la plataforma "+version[1])
+    match packageType:
+        case "source":
+            sourceName="GestorDePacientesRadioterapia"+"-"+version[0].replace("v", "")+".zip"
+            urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/archive/refs/tags/"+version[0]+".zip", "source.zip")
+        case "compiled":
+            if version[1]=="Linux":
+                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/linuxRelease.zip", "release.zip")
+            elif version[1]=="Windows":
+                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/winRelease.zip", "release.zip")
--- scripts/update.py
+++ scripts/update.py
@@ -1,22 +1,7 @@
 import requests
 from platform import system
 from urllib.request import urlretrieve
-
-def update(packageType, version="latest"):
-    if version=="latest":
-        version = (requests.get("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest").url.split("/").pop(), system())
-    else:
-        version = (version, system())
-    print("Descargando version "+version[0]+" para la plataforma "+version[1])
-    match packageType:
-        case "source":
-            sourceName="GestorDePacientesRadioterapia"+"-"+version[0].replace("v", "")+".zip"
-            urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/archive/refs/tags/"+version[0]+".zip", "source.zip")
-        case "compiled":
-            if version[1]=="Linux":
-                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/linuxRelease.zip", "release.zip")
-            elif version[1]=="Windows":
-                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/winRelease.zip", "release.zip")
+from libs.funcs import update
 
 if __name__=="__main__":
     version=input("Descargar version (dejar en blanco para la última): ")
