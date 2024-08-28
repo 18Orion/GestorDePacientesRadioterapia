65e0ed6a7dff981f0c4b5597efd883387b64c1a9
Sergio <sergiominanor@gmail.com>
2024-08-28 10:34:20 +0200

    Update script finished

--- scripts/update.py
+++ scripts/update.py
@@ -1,9 +1,29 @@
 import requests
 from platform import system
 from urllib.request import urlretrieve
-version = (requests.get("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest").url.split("/").pop(), system())
-print("Descargando version "+version[0]+" para la plataforma "+version[1])
-if version[1]=="Linux":
-    urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/linuxRelease.zip", "release.zip")
-elif version[1]=="Windows":
-    urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/winRelease.zip", "release.zip")
+
+def update(packageType, version="latest"):
+    if version=="latest":
+        version = (requests.get("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest").url.split("/").pop(), system())
+    else:
+        version = (version, system())
+    print("Descargando version "+version[0]+" para la plataforma "+version[1])
+    match packageType:
+        case "source":
+            sourceName="GestorDePacientesRadioterapia"+"-"+version[0].replace("v", "")+".zip"
+            urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/archive/refs/tags/"+version[0]+".zip", "source.zip")
+        case "compiled":
+            if version[1]=="Linux":
+                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/linuxRelease.zip", "release.zip")
+            elif version[1]=="Windows":
+                urlretrieve("https://github.com/18Orion/GestorDePacientesRadioterapia/releases/latest/download/winRelease.zip", "release.zip")
+
+if __name__=="__main__":
+    version=input("Descargar version (dejar en blanco para la última): ")
+    if not(version):
+        version="latest"
+    match int(input("Descargar 1. Paquete o 2. Source: ")):
+        case 1:
+            update("compiled", version)
+        case 2:
+            update("source", version)
